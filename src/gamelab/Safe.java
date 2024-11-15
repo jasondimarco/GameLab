d4711490ee2e36d986769e8e773b3f033ab48d03
dimarco <dimarco@mariner.cs.widener.edu>
2024-11-15 13:54:27 -0500

    updated task 3

--- src/gamelab/Safe.java
+++ src/gamelab/Safe.java
@@ -5,15 +5,17 @@ public class Safe extends Item {
 	public Safe(String n, String d) {
 		super(n, d);
 	}
-	
+
 	@Override
 	public void open() {
-		if (Game.inv.get(combination).equals(null)) {
-			Item diamond = new Item("diamond", "Shiny.");
-			Game.inv.add(diamond);
-			Game.print("Using the combination, you open the safe and find a diamond inside! Naturally, you pocket the diamond.");
-		} else {
-			Game.print("The safe is locked and you don't have the combination.");
-		}
+		for (Item c : Game.inv)
+			if (c.getName().equals("Combination")) {
+				Item diamond = new Item("diamond", "Shiny.");
+				Game.inv.add(diamond);
+				Game.print(
+						"Using the combination, you open the safe and find a diamond inside! Naturally, you pocket the diamond.");
+				return;
+			}
+		Game.print("The safe is locked and you don't have the combination.");
 	}
 }
